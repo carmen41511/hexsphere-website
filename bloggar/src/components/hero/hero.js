1c73155fc21ff574755e59a37d8afd2fa93208e3
NotC <jiawl22@uci.edu>
2023-09-24 23:49:03 -0400

    added related post to hero.js

--- bloggar/src/components/hero/hero.js
+++ bloggar/src/components/hero/hero.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { Link } from 'react-router-dom'
 import heroBlog from '../../api/heroBlogs'
+import Highlight from '../../api/Highlight'
+
 
 
 
 const Hero = () => {
+
+    const ClickHandler = (props) => {
+        window.scrollTo(10, 0);
+    }
+
     return (
         <div className="wpo-blog-hero-area">
             <div className="container">
@@ -30,7 +37,42 @@ const Hero = () => {
                                         </div>
                                     </div>
                                 ))}
+                                
+                                {/* <div className={`col col-lg-4 col-12 ${props.hideClass}`}> */}
+                                <div className="blog-sidebar">
+                                {/* <div className="widget category-widget"> */}
+                                {   /* <h3>Post Categories</h3>
+                                    <ul>
+                                        <li><Link onClick={ClickHandler} to="/blog">Lifestyle<span>(05)</span></Link></li>
+                                        <li><Link onClick={ClickHandler} to="/blog">Inspiration <span>(07)</span></Link></li>
+                                        <li><Link onClick={ClickHandler} to="/blog">Fashion <span>(03)</span></Link></li>
+                                        <li><Link onClick={ClickHandler} to="/blog">Photography <span>(06)</span></Link></li>
+                                        <li><Link onClick={ClickHandler} to="/blog">Travel <span>(12)</span></Link></li>
+                                        <li><Link onClick={ClickHandler} to="/blog">Business <span>(08)</span></Link></li>
+                                    </ul> */}
+                                {/* </div> */}
+                                <div className="widget recent-post-widget">
+                                    <h3>Related Posts</h3>
+                                    {Highlight.slice(0, 5).map((blog, bitem) => (
+                                        <div className="posts" key={bitem}>
+                                            <div className="post">
+                                                <div className="img-holder">
+                                                    <img src={blog.screens} alt />
+                                                </div>
+                                                <div className="details">
+                                                    <span className="date">{blog.create_at} </span>
+                                                    <h4><Link onClick={ClickHandler} to={`/highlight-single/${blog.slug}`}>{blog.title}</Link></h4>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ))}
 
+                                </div>
+                                {/* <div className="wpo-add-widget">
+                                    <Link to="/"><img src={add} alt=""/></Link>
+                                </div> */}
+                        </div>
+                    </div>
 
 
                                 {/* <div className="grid s2">
@@ -50,11 +92,17 @@ const Hero = () => {
                                 </div> */}
                             </div>
                         {/* </div> */}
+
+
+                        
+
                     </div>
+
+                    
                 {/* </div> */}
 
             </div>
-        </div>
+        // </div>
     )
 }
 
